b06601d7b92f324f6337e2ef8460894d8c0122ef
anbusto <anbusto@sandia.gov>
2023-07-18 10:12:41 -0600

    updated config

--- docs/build_files/conf.py
+++ docs/build_files/conf.py
@@ -5,9 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+# import os
+# import sys
+import sphinx_bootstrap_theme
+# sys.path.insert(0, os.path.abspath('..'))
 
 project = 'My Awesome PyRIID Project'
 copyright = '2023, Alyshia N. Bustos'
@@ -25,5 +26,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
