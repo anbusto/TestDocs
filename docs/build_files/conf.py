1a1bc47cfcb4c9b81a7ac9b1815982f9bebc643c
anbusto <anbusto@sandia.gov>
2023-07-18 08:34:50 -0600

    updated things

--- docs/build_files/conf.py
+++ docs/build_files/conf.py
@@ -29,4 +29,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ['./_static']
