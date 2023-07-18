02144882c09de31069819de6ccb8b7eb0d623ce0
anbusto <anbusto@sandia.gov>
2023-07-18 08:29:41 -0600

    updated styles again

--- docs/build_files/conf.py
+++ docs/build_files/conf.py
@@ -7,6 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'My Awesome PyRIID Project'
@@ -25,5 +26,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
